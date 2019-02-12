eff57d0447680cade7eb566631452856e7ec3ac8
Tim Dingman <me@timdingman.com>
2019-02-12 00:44:12 +0000

    Relates to #4

--- roche/core/urls.py
+++ roche/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        #path('', views.RocheListView.as_view(), name='index'),
-        #path('<id:pk>', views.RocheDetailView.as_view(), name='roche'),
+        path('', views.RocheListView.as_view(), name='index'),
+        path('<int:pk>/', views.RocheDetailView.as_view(), name='roche'),
         # submit
         ]
--- roche/core/views.py
+++ roche/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from core.models import Roche
 
-# Create your views here.
+class RocheListView(generic.ListView):
+    model = Roche
+
+class RocheDetailView(generic.DetailView):
+    model = Roche
